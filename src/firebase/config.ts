ac86276ec5f3b8ec5fe20a89ad1d0e3615173919
Lucas Coutinho <cucasloutinho@gmail.com>
2025-12-02 19:13:16 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
-// IMPORTANT: Replace this with your actual Firebase config object
-// You can get this from the Firebase console
+// IMPORTANT: This file is auto-generated. Do not modify.
 export const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_APP_ID",
+  projectId: "studio-8751194365-e1851",
+  appId: "1:1077506914734:web:5b177bc58b0bf7698d703f",
+  apiKey: "AIzaSyCiCX96m2emgH-uLw0mIrnfijgrFpeZmgk",
+  authDomain: "studio-8751194365-e1851.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "1077506914734",
+  storageBucket: "studio-8751194365-e1851.appspot.com",
 };
