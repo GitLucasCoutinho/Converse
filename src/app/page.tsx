5e344f99b38fba05f7b8764a7a84a819f6e320f3
Lucas Coutinho <cucasloutinho@gmail.com>
2025-12-02 17:17:07 +0000

    Runtime Error
    Hydration failed because the server rendered HTML didn't

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 import { ChatLayout } from "@/components/converse/chat-layout";
 import { Translator } from "@/components/converse/translator";
+import { ClientOnly } from "@/components/client-only";
 
 export default function Home() {
   return (
     <div className="flex h-screen w-full flex-col items-center bg-background p-4 gap-4 lg:flex-row lg:items-start lg:justify-center">
       <div className="w-full h-full lg:w-1/2 lg:max-w-3xl">
-        <ChatLayout />
+        <ClientOnly>
+          <ChatLayout />
+        </ClientOnly>
       </div>
       <div className="w-full h-full lg:w-1/2 lg:max-w-3xl">
-        <Translator />
+        <ClientOnly>
+          <Translator />
+        </ClientOnly>
       </div>
     </div>
   );
