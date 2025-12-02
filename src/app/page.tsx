3ed625e596acecb6379b112ab1fbab80d382a240
Lucas Coutinho <cucasloutinho@gmail.com>
2025-12-02 17:45:09 +0000

    eu vi que em modo tela mais cumprida como de smartphone o campo de conve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ import { ClientOnly } from "@/components/client-only";
 export default function Home() {
   return (
     <div className="flex h-screen w-full flex-col items-center bg-background p-4 gap-4 lg:flex-row lg:items-start lg:justify-center">
-      <div className="w-full h-full lg:w-1/2 lg:max-w-3xl">
+      <div className="w-full flex-[2] overflow-hidden lg:max-w-3xl lg:h-full">
         <ClientOnly>
           <ChatLayout />
         </ClientOnly>
       </div>
-      <div className="w-full h-full lg:w-1/2 lg:max-w-3xl">
+      <div className="w-full flex-[1] overflow-hidden lg:max-w-3xl lg:h-full">
         <ClientOnly>
           <Translator />
         </ClientOnly>
