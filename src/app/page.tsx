fa4544bc841151baf12e03e31c2d2ee75e561edc
Lucas Coutinho <cucasloutinho@gmail.com>
2025-12-02 17:47:06 +0000

    volta como era antes dessa alteracao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ import { ClientOnly } from "@/components/client-only";
 export default function Home() {
   return (
     <div className="flex h-screen w-full flex-col items-center bg-background p-4 gap-4 lg:flex-row lg:items-start lg:justify-center">
-      <div className="w-full flex-[2] overflow-hidden lg:max-w-3xl lg:h-full">
+      <div className="w-full h-full lg:max-w-3xl">
         <ClientOnly>
           <ChatLayout />
         </ClientOnly>
       </div>
-      <div className="w-full flex-[1] overflow-hidden lg:max-w-3xl lg:h-full">
+      <div className="w-full h-full lg:max-w-3xl">
         <ClientOnly>
           <Translator />
         </ClientOnly>
